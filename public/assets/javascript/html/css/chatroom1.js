3d90246c429c724460337333642d5c99a8400f55
Nick Duran <nvduran@gmail.com>
2021-04-01 21:32:18 -0500

    basic chatroom

--- public/assets/javascript/html/css/chatroom1.js
+++ public/assets/javascript/html/css/chatroom1.js
@@ -2,7 +2,7 @@ var chatBox = document.getElementById("chatBox")
 
 document.getElementById("send-chat").addEventListener("click", function() {
      var x = document.getElementById("chatInput").value;
-     alert(x);
+     // alert(x);
      chatBox.innerHTML = x;
    });
 
