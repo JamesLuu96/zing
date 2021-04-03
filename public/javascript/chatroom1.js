1ef8433d63251df2177a50854a77a3c4764b70c2
fasikaWalle <fasikabini12@gmail.com>
2021-04-02 20:59:14 -0500

    getting user data from session

--- public/javascript/chatroom1.js
+++ public/javascript/chatroom1.js
@@ -1,18 +1,32 @@
 var chatBox = document.getElementById("chatBox")
+const {username,room}=JSON.parse(sessionStorage.getItem("userInfo"))
 
-document.getElementById("send-chat").addEventListener("click", function () {
-     var x = document.getElementById("chatInput").value;
+var socket=io();
+
+socket.emit('joinRoom',{username,room}) 
+
+socket.on('roomUsers',({room,users})=>{
+     displayRoomName(room);
+     displayUsers(users)
+ })
+ 
+function sendMessage () {
+     var message = document.getElementById("chatInput").value;
      // alert(x);
      // chatBox.innerHTML = x;
 
      var node = document.createElement("LI");                 // Create a <li> node
-     var textnode = document.createTextNode(x);         // Create a text node
+     var textnode = document.createTextNode(message);         // Create a text node
      node.appendChild(textnode);                              // Append the text to <li>
      document.getElementById("chatList").appendChild(node);     // Append <li> to <ul> with id="myList"
-});
+};
 
 
 var node = document.createElement("LI");                 // Create a <li> node
-var textnode = document.createTextNode(x);         // Create a text node
+var textnode = document.createTextNode(message);         // Create a text node
 node.appendChild(textnode);                              // Append the text to <li>
 document.getElementById("chatList").appendChild(node);     // Append <li> to <ul> with id="myList"
+
+
+
+document.getElementById("send-chat").addEventListener("click",sendMessage)
