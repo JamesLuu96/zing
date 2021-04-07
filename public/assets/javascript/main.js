6c0b9340c45cca4f993a11a42aaa4d2dcd37dd87
flokamp <74511633+flokamp@users.noreply.github.com>
2021-04-06 21:25:28 -0500

    updated text alternations

--- public/assets/javascript/main.js
+++ public/assets/javascript/main.js
@@ -23,11 +23,11 @@ function alterMessage(chatEl) {
 		chatEl.classList.add("tiny");
 	} else if (type_name === "angry") {
 		chatEl.classList.add("angry");
-	} else if (type_name === "the upside down") {
-		chatEl.classList.add("the-upside-down");
-	} else if (type_name === "anonymous") {
+	} else if (type_name === "backwards") {
+		chatEl.classList.add("backwards");
+	} else if (type_name === "excited") {
 		chatEl.classList.add("excited");
-	} else if (type_name === "pig latin") {
+	} else if (type_name === "blurry") {
 		chatEl.classList.add("blurry");
 	}
 }
