d377e04a554f5a04dab5c24552f41c96a16ebb96
Nick Duran <nvduran@gmail.com>
2021-04-06 20:48:57 -0500

    connections

--- config/connection.js
+++ config/connection.js
@@ -6,11 +6,7 @@ let sequelize;
 if (process.env.JAWSDB_URL) {
 	sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
-<<<<<<< HEAD
 	sequelize = new Sequelize("chat_db", "root", "!6MUOweit", {
-=======
-	sequelize = new Sequelize("chat_db", "root", "Crosstale94", {
->>>>>>> feature/typing
 		host: "localhost",
 		dialect: "mysql",
 		port: 3306,
